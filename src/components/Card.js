34b5b50bea488e1f16f2b5bf6c9853be5cb3ad98
Airam <airam.dev@gmail.com>
2021-04-29 18:03:32 +0100

    v1.0

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Card.css'
+import github from './icons/github.png';
+import linkedin from './icons/linkedin.png';
+import email from './icons/email.png';
+
+export const Card = () => {
+    return (
+        <div id="card">
+            <div><h1>Airam</h1></div>
+            <div><h2>Web Developer</h2></div>
+            <div id="socialMedia">
+                <a href="https://github.com/Earkh"><img src={github} alt="github logo" /></a>
+                <a href="https://www.linkedin.com/in/airam-caraballo-santana/"><img src={linkedin} alt="linkedin logo" /></a>
+                <a href="mailto:airam.dev@gmail.com"><img src={email} alt="email logo" /></a>
+            </div>
+        </div>
+    )
+}
